b06905a6b185fcbddede90c7f810078f5138ee11
Jasmine Dahilig <jasmine@dahilig.com>
2020-01-05 14:50:23 -0800

    use boltdb for persistent storage

--- Task.go
+++ Task.go
@@ -3,20 +3,37 @@ package main
 import "time"
 
 type Task struct {
+	Uuid      string
 	Name      string
 	Completed bool
-	//description string
-	//parent      Task
-	//children    []Task
-	//tags        []string
-	//recurrence  Recurrence
-	//estimate    time.Duration
-	//priority    int
-	//createdAt   time.Time
-	//completedAt time.Time
+
+	//CreatedAt   time.Time
+	//CompletedAt time.Time
+
+	//Description string
+	//Priority    int
+	//Estimate    time.Duration
+
+	//Recurrence  Recurrence
+
+	// Dependants   []*Task // The parent depends on the child
+	// Dependencies []*Task
+
+	//Tags        []string
 }
 
 // calculate next action
+// - Navigate graph
+// - Priority weighting
+// - Detect catch-22s (cycles)
+// func (t *Task) getNextAction() *Task {
+// 	for _, d := range t.Dependencies {
+// 		if !d.Completed {
+// 			return d
+// 		}
+// 	}
+// 	return nil
+// }
 
 type Recurrence struct {
 	frequency time.Duration
--- main.go
+++ main.go
@@ -3,63 +3,102 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/google/uuid"
 )
 
 // start the server
 var tasks = []Task{}
+var db *bolt.DB
+
+const TASKS_BUCKET = "Tasks"
 
 func main() {
-	fmt.Println("Starting go.todo server")
-	fmt.Println("Go to http://localhost:8080 to get to it")
+	log.Println("Starting go.todo server")
+
+	log.Println("Starting boltdb")
+	dbTmp, err := bolt.Open("todo.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = dbTmp
+	defer db.Close()
+
+	// Create Tasks bucket
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(TASKS_BUCKET))
+		if err != nil {
+			return fmt.Errorf("Error creating bucket: %s", err)
+		}
+		return nil
+	})
+
+	log.Println("Go to http://localhost:8080 to get to it")
 	http.HandleFunc("/", RootPage)
 	http.HandleFunc("/task", TaskPage)
 	http.ListenAndServe(":8080", nil)
-}
-
-func RootPage(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "BOOYAH\n")
 }
 
 func TaskPage(writer http.ResponseWriter, request *http.Request) {
 	decoder := json.NewDecoder(request.Body)
 	var task Task
 	decoder.Decode(&task)
-	fmt.Println(task)
 
 	// If task struct is not empty
 	// https://stackoverflow.com/a/28447372/1766434
 	if (Task{}) != task {
-		// If task is already in list, Update it
-		updated := false
-		for i, t := range tasks {
-			if t.Name == task.Name {
-				tasks[i] = task
-				updated = true
-			}
+		if task.Uuid == "" {
+			task.Uuid = uuid.New().String()
 		}
-		// Otherwise, Add it
-		if !updated {
-			tasks = append(tasks, task)
+
+		encoded, err := json.Marshal(task)
+		if err != nil {
+			log.Printf("Error marshalling task: %s\n", err)
 		}
+
+		db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(TASKS_BUCKET))
+			err := b.Put([]byte(task.Uuid), []byte(encoded))
+			fmt.Printf("Updated task: %s\n", task)
+			return err
+		})
+
 	}
-	tasksHtml := makeTasksHtml()
+	tasksHtml := fmt.Sprintf("Created task: %s", task)
 	fmt.Fprintf(writer, tasksHtml)
 }
 
-func makeTasksHtml() string {
+func RootPage(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Formatting tasks...")
 	html := ""
-	for _, t := range tasks {
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(TASKS_BUCKET))
+		c := b.Cursor()
 
-		// Because Go language doesn't have ternary operators
-		// https://golang.org/doc/faq#Does_Go_have_a_ternary_form
-		completedMarker := " "
-		if t.Completed {
-			completedMarker = "x"
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			fmt.Printf("Uuid %s\n", k)
+			var t Task
+			err := json.Unmarshal(v, &t)
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+
+			// Because Go language doesn't have ternary operators
+			// https://golang.org/doc/faq#Does_Go_have_a_ternary_form
+			completedMarker := " "
+			if t.Completed {
+				completedMarker = "x"
+			}
+
+			html = html + fmt.Sprintf("- [%s] ", completedMarker)
+			html = html + fmt.Sprintf("%s\n", t.Name)
 		}
-
-		html = html + fmt.Sprintf("- [%s] ", completedMarker)
-		html = html + fmt.Sprintf("%s\n", t.Name)
-	}
-	return html
+		fmt.Fprintf(writer, html)
+		return nil
+	})
 }
